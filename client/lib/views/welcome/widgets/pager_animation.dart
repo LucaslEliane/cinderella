d6dddc399e804470a2c1438fbd787317313fe851
lucasleliane <517197934@qq.com>
2019-07-15 21:51:11 +0800

    feat(welcome): welcome page drag animation

--- client/lib/views/welcome/widgets/pager_animation.dart
+++ client/lib/views/welcome/widgets/pager_animation.dart
@@ -1,18 +1,70 @@
+import 'dart:ui';
+
+import 'package:client/views/welcome/widgets/pager_slider.dart';
 import 'package:flutter/material.dart';
 
 class DragAnimation {
-  static const ANIMATED_PERCENT_PER_MILISECOND = 0.05;
+  static const ANIMATED_PERCENT_PER_MILISECOND = 0.005;
 
   AnimationController welcomeDragCompletedController;
 
-  final startSlidePercent;
   final slideDirection;
 
-
   DragAnimation({
-    this.startSlidePercent,
-    this.slideDirection
+    this.slideDirection,
+    slidePercent,
+    slidePercentGoal,
+    sliderUpdater,
+    TickerProvider vsync
   }) {
-    
+    double slideRemaining;
+    if (slidePercentGoal == 1) {
+      slideRemaining = 1 - slidePercent;
+    } else {
+      slideRemaining = slidePercent;
+    }
+
+    final duration = new Duration(
+      milliseconds: (slideRemaining / ANIMATED_PERCENT_PER_MILISECOND).round().abs()
+    );
+
+    welcomeDragCompletedController = new AnimationController(
+      duration: duration,
+      vsync: vsync
+    )
+    ..addListener(() {
+      slidePercent = lerpDouble(
+        slidePercent,
+        slidePercentGoal,
+        welcomeDragCompletedController.value
+      );
+
+      sliderUpdater.add(
+        new SliderState(
+          slideDirection,
+          slidePercent,
+          SliderEventType.animating
+        )
+      );
+    })
+    ..addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        sliderUpdater.add(
+          new SliderState(
+            slideDirection,
+            slidePercent,
+            SliderEventType.doneAnimating
+          )
+        );
+      }
+    });
+  }
+
+  run() {
+    welcomeDragCompletedController.forward(from: 0.0);
+  }
+
+  dispose() {
+    welcomeDragCompletedController.dispose();
   }
 }
--- client/lib/views/welcome/widgets/pages.dart
+++ client/lib/views/welcome/widgets/pages.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:client/utils/screen_factor.dart';
+import 'package:client/views/welcome/widgets/pager_animation.dart';
 import 'package:flutter/material.dart';
 import 'package:client/view_models/welcome.dart';
 import 'package:client/views/welcome/widgets/pager_indicator.dart';
@@ -9,20 +10,22 @@ import 'package:client/views/welcome/widgets/pager_dragger.dart';
 
 final pages = [
   PageViewModel(const Color(0xFFcd344f), 'assets/images/welcome/page1.png'),
-  PageViewModel(const Color(0xFFce423a), 'assets/images/welcome/page2.png')
+  PageViewModel(const Color(0xFFce423a), 'assets/images/welcome/page2.jpeg')
 ];
 
 class Pages extends StatefulWidget {
   @override
   PagesState createState() => new PagesState();
 }
-class PagesState extends State<Pages> {
+class PagesState extends State<Pages> with TickerProviderStateMixin {
   StreamController sliderUpdater;
   int activeIndex = 0;
+  int maxIndex = pages.length;
   SlideDirection slideDirection = SlideDirection.none;
   double slidePercent = 0.0;
+  DragAnimation dragAnimation;
 
-  final PageViewModel viewModel = pages[0];
+  PageViewModel viewModel = pages[0];
 
   PagesState() {
     sliderUpdater = new StreamController<SliderState>();
@@ -30,24 +33,77 @@ class PagesState extends State<Pages> {
     sliderUpdater.stream.listen(onDrag);
   }
 
+  SlideDirection reverseSlideDirection(SlideDirection slideDirection) {
+    if (slideDirection == SlideDirection.leftToRight) {
+      return SlideDirection.rightToLeft;
+    }
+    if (slideDirection == SlideDirection.rightToLeft) {
+      return SlideDirection.leftToRight;
+    }
+    return SlideDirection.none;
+  }
+
   void onDrag(dynamic event) {
     SliderState state = event;
     if (mounted) {
       setState(() {
-        final eventType = state.sliderEventType;
+        final SliderEventType eventType = state.sliderEventType;
+        final double currentSlidePercent = state.slidePercent;
+        final SlideDirection currentDirection = state.direction;
 
         switch (eventType) {
           case SliderEventType.dragging:
-            slideDirection = state.direction;
-            slidePercent = state.slidePercent;
+            slideDirection = currentDirection;
+            slidePercent = this.getNextSliderPercent(currentDirection, currentSlidePercent);
             break;
           case SliderEventType.doneDragging:
-            if (slidePercent > 0.5) {
-
+            final currentSlidePercent = state.slidePercent;
+            if (currentSlidePercent > 0.5) {
+              dragAnimation = new DragAnimation(
+                slideDirection: currentDirection,
+                slidePercent: currentSlidePercent,
+                slidePercentGoal: 1,
+                sliderUpdater: sliderUpdater,
+                vsync: this
+              );
+            } else {
+              dragAnimation = new DragAnimation(
+                slideDirection: currentDirection,
+                slidePercent: currentSlidePercent,
+                slidePercentGoal: 0,
+                sliderUpdater: sliderUpdater,
+                vsync: this
+              );
             }
+
+            dragAnimation.run();
+            break;
+          case SliderEventType.doneAnimating:
+            if (currentDirection == SlideDirection.leftToRight && activeIndex < maxIndex - 1) {
+              activeIndex += 1;
+            }
+            if (currentDirection == SlideDirection.rightToLeft && activeIndex > 0) {
+              activeIndex -= 1;
+            }
+            viewModel = pages[activeIndex];
+            break;
+          case SliderEventType.animating:
+            slideDirection = currentDirection;
+            slidePercent = this.getNextSliderPercent(currentDirection, currentSlidePercent);
+            break;
         }
       });
     }
+  }
+
+  double getNextSliderPercent(SlideDirection slideDirection, double slidePercent) {
+    if (slideDirection == SlideDirection.leftToRight) {
+      return -slidePercent;
+    }
+    if (slideDirection == SlideDirection.rightToLeft) {
+      return slidePercent;
+    }
+    return 0;
   }
 
   @override
@@ -74,6 +130,7 @@ class PagesState extends State<Pages> {
   }
 
   Column layout(BuildContext context) {
+
     // 类似于 CSS 中的 flex 布局，通过主轴和交叉轴进行布局
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
